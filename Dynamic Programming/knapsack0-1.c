61039e086907c87b5ddad24cc995e491bc55dceb
dhanurk <dhanurk712@gmail.com>
2016-11-15 02:06:20 +0530

    bottom up knapsack 0-1

---
+++ Dynamic Programming/knapsack0-1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int knapsack(int capacity, int size, int *weight, int *value);
+int max(int a, int b);
+
+int main(){
+  system("clear");
+  int size;
+  printf("Enter size of knapsack: ");
+  scanf("%d", &size);
+  int weight[size], value[size];
+  for(int i = 0; i < size; i++) {
+    printf("Enter value and weight for item %d respectively.\n", i + 1);
+    scanf("%d%d", &value[i], &weight[i]);
+  }
+  int capacity;
+  printf("Enter knapsack capacity: ");
+  scanf("%d", &capacity);
+  int ans = 0;
+  ans = knapsack(capacity, size - 1, weight, value);
+  printf("Maximum value of knapsack for the given capacity is: %d\n", ans);
+  printf("Press enter to exit.\n");
+  char b;
+  scanf("%c", &b);
+  scanf("%c", &b);
+}
+
+int max(int a, int b) {
+  return a > b ? a : b;
+}
+
+int knapsack(int capacity, int size, int *weight, int *value) {
+  if(size == 0 || weight == 0)
+    return 0;
+  if(capacity - weight[size] < 0)
+    return knapsack(capacity, size - 1, weight, value);
+  return(max(knapsack(capacity, size - 1, weight, value), value[size] + knapsack(capacity - weight[size], size - 1, weight, value)));
+}
